bd5326b798a16a4dcc75fbc0a1207b4046109d56
trangel <trangel@lbl.gov>
2017-09-11 13:14:01 -0700

    flask app

---
+++ flaskapp/data/update.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+cp ../../data/AutismParentMagazine-posts-tokens.csv .
+cp ../../data/lsi-model.save .
+cp ../../data/lsi-model.projection .
+cp ../../data/tfidf.save .
+cp ../../data/dictionary.save .
+cp ../../data/lsi-matsim.save .
---
+++ flaskapp/views.py
@@ -0,0 +1,76 @@
+from flask import render_template,request
+from flaskapp import app
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+import pandas as pd
+import psycopg2
+
+# Import "flaskapp/query_model.py"
+import os
+import sys
+path2flaskapp=os.path.join(os.getcwd(),"flaskapp")
+sys.path.insert(0, path2flaskapp)
+from query_model import query_model
+
+user = 'rangel' #add your username here (same as previous postgreSQL)            
+host = 'localhost'
+dbname = 'birth_db'
+db = create_engine('postgres://%s%s/%s'%(user,host,dbname))
+con = None
+con = psycopg2.connect(database = dbname, user = user)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html",
+       title = 'Home', user = { 'nickname': 'Miguel' },
+       )
+
+@app.route('/db')
+def birth_page():
+    sql_query = """                                                             
+                SELECT * FROM birth_data_table WHERE delivery_method='Cesarean'\
+;                                                                               
+                """
+    query_results = pd.read_sql_query(sql_query,con)
+    births = ""
+    print ( query_results[:10] )
+    for i in range(0,10):
+        births += query_results.iloc[i]['birth_month']
+        births += "<br>"
+    return births
+
+@app.route('/db_fancy')
+def AutismExpert_page_fancy():
+    sql_query = """
+               SELECT index, attendant, birth_month FROM birth_data_table WHERE delivery_method='Cesarean';
+                """
+    query_results=pd.read_sql_query(sql_query,con)
+    births = []
+    for i in range(0,query_results.shape[0]):
+        births.append(dict(index=query_results.iloc[i]['index'], attendant=query_results.iloc[i]['attendant'], birth_month=query_results.iloc[i]['birth_month']))
+    return render_template('AutismExpert.html',births=births)
+
+@app.route('/input')
+def AutismExpert_input():
+    return render_template("input.html")
+
+@app.route('/output')
+def AutismExpert_output():
+  #pull 'birth_month' from input field and store it
+  text = request.args.get('autism_query')
+
+# Find similar articles from model
+  the_result=query_model(text)
+
+
+#    #just select the Cesareans  from the birth dtabase for the month that the user inputs
+#  query = "SELECT index, attendant, birth_month FROM birth_data_table WHERE delivery_method='Cesarean' AND birth_month='%s'" % patient
+#  print ( query )
+#  query_results=pd.read_sql_query(query,con)
+#  print ( query_results )
+  births = []
+#  for i in range(0,query_results.shape[0]):
+#      births.append(dict(index=query_results.iloc[i]['index'], attendant=query_results.iloc[i]['attendant'], birth_month=query_results.iloc[i]['birth_month']))
+#      the_result = ModelIt(patient,births)
+  return render_template("output.html", births = births, the_result = the_result)
