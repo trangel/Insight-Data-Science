c50327111fbf91bc7ae142b01bc6fc0ae06f5af4
trangel <trangel@lbl.gov>
2017-09-14 14:42:13 -0700

    sql query

--- flaskapp/views.py
+++ flaskapp/views.py
@@ -50,14 +50,16 @@ def AutismExpert_output():
   result_forums, result_articles = query_model(text)
 
 
-#    #just select the Cesareans  from the birth dtabase for the month that the user inputs
-#  query = "SELECT index, attendant, birth_month FROM birth_data_table WHERE delivery_method='Cesarean' AND birth_month='%s'" % patient
-#  print ( query )
-#  query_results=pd.read_sql_query(query,con)
-#  print ( query_results )
-  births = []
-#  for i in range(0,query_results.shape[0]):
-#      births.append(dict(index=query_results.iloc[i]['index'], attendant=query_results.iloc[i]['attendant'], birth_month=query_results.iloc[i]['birth_month']))
-#      the_result = ModelIt(patient,births)
-  return render_template("output.html", births = births, result_forums = result_forums, result_articles = result_articles)
+  
+  #article_ids=",".join(article_ids)
+ 
+  query = "SELECT title, text, href FROM \"articles-n-forums-posts\" WHERE index = 2 "
+  print ( query )
+  query_results=pd.read_sql_query(query,con)
+  print ( query_results )
+  autism_docs = []
+  for i in range(0,query_results.shape[0]):
+      autism_docs.append(dict(title=query_results.iloc[i]['title'], href=query_results.iloc[i]['href'], text=query_results.iloc[i]['text']))
+
+  return render_template("output.html", autism_docs=autism_docs, result_forums = result_forums, result_articles = result_articles)
 
