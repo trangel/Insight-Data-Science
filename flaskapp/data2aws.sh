762cadc469b5d3a395959f9450783cf738de787a
trangel <trangel@lbl.gov>
2017-09-22 15:40:59 -0700

    web app runing great!

--- flaskapp/data2aws.sh
+++ flaskapp/data2aws.sh
@@ -1,15 +1,17 @@
-scp -i ../aws/autismxpert.pem views.py ubuntu@13.59.215.199://home/ubuntu/Insight/flaskapp/.
-scp -i ../aws/autismxpert.pem query_model.py ubuntu@13.59.215.199://home/ubuntu/Insight/flaskapp/.
-scp -i ../aws/autismxpert.pem get_questions.py ubuntu@13.59.215.199://home/ubuntu/Insight/flaskapp/.
+#scp -i ../aws/autismxpert.pem views.py ubuntu@13.59.215.199://home/ubuntu/Insight/flaskapp/.
+#scp -i ../aws/autismxpert.pem query_model.py ubuntu@13.59.215.199://home/ubuntu/Insight/flaskapp/.
+#scp -i ../aws/autismxpert.pem get_questions.py ubuntu@13.59.215.199://home/ubuntu/Insight/flaskapp/.
+
+#scp -i ../aws/autismxpert.pem -r static ubuntu@13.59.215.199://home/ubuntu/Insight/flaskapp/.
 
 #cd data
 #bash data2aws.sh
 #cd ..
 #
 
-cd static
-bash data2aws.sh
-cd ..
+#cd static
+#bash data2aws.sh
+#cd ..
 
 cd templates
 bash data2aws.sh
