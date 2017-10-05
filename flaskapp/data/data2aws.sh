f758569f18c2d91c0c9986d93fb5e064812ef192
trangel <trangel@lbl.gov>
2017-10-04 19:33:56 -0700

    final version of demo

--- flaskapp/data/data2aws.sh
+++ flaskapp/data/data2aws.sh
@@ -1,12 +1,13 @@
 ## model
-scp -i ../../aws/autismxpert.pem lsi* ubuntu@13.59.215.199://home/ubuntu/Insight/flaskapp/data/.
-scp -i ../../aws/autismxpert.pem tdidf.save ubuntu@13.59.215.199://home/ubuntu/Insight/flaskapp/data/.
-scp -i ../../aws/autismxpert.pem dictionary.save ubuntu@13.59.215.199://home/ubuntu/Insight/flaskapp/data/.
+#scp -i ../../aws/autismxpert.pem lsi* ubuntu@13.59.215.199://home/ubuntu/Insight/flaskapp/data/.
+#scp -i ../../aws/autismxpert.pem tdidf.save ubuntu@13.59.215.199://home/ubuntu/Insight/flaskapp/data/.
+#scp -i ../../aws/autismxpert.pem dictionary.save ubuntu@13.59.215.199://home/ubuntu/Insight/flaskapp/data/.
+#
+## dataframe
+#scp -i ../../aws/autismxpert.pem *csv ubuntu@13.59.215.199://home/ubuntu/Insight/flaskapp/data/.
+#
+## questions
+#scp -i ../../aws/autismxpert.pem questions.dat ubuntu@13.59.215.199://home/ubuntu/Insight/flaskapp/data/.
 
-# dataframe
-scp -i ../../aws/autismxpert.pem *csv ubuntu@13.59.215.199://home/ubuntu/Insight/flaskapp/data/.
-
-# questions
-scp -i ../../aws/autismxpert.pem questions.dat ubuntu@13.59.215.199://home/ubuntu/Insight/flaskapp/data/.
-
-
+# movie
+#scp -i ../../aws/autismxpert.pem AutismXpert.mov ubuntu@13.59.215.199://home/ubuntu/Insight/flaskapp/data/.
--- flaskapp/static/data2aws.sh
+++ flaskapp/static/data2aws.sh
@@ -1,5 +1,5 @@
 
-scp -i ../../aws/autismxpert.pem *.png ubuntu@13.59.215.199://home/ubuntu/Insight/flaskapp/static/.
+#scp -i ../../aws/autismxpert.pem *.png ubuntu@13.59.215.199://home/ubuntu/Insight/flaskapp/static/.
 scp -i ../../aws/autismxpert.pem *.mov ubuntu@13.59.215.199://home/ubuntu/Insight/flaskapp/static/.
 
 #scp -i ../../aws/autismxpert.pem -r * ubuntu@13.59.215.199://home/ubuntu/Insight/flaskapp/static/.
